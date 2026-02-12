d14acfb7e3a4b1faa7a62c67a640721daedd7aa9
Justin <justin.le@berkeley.edu>
2026-02-11 20:57:19 -0800

    backend

---
+++ inventory-server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+let inventory = [
+  {
+    id: 1,
+    name: "Arduino Kit",
+    category: "Hardware",
+    quantity: 5,
+    status: "Available"
+  },
+  {
+    id: 2,
+    name: "Figma License",
+    category: "Software",
+    quantity: 20,
+    status: "Available"
+  }
+];
+
+app.get("/inventory", (req, res) => {
+  res.json(inventory);
+});
+
+app.post("/inventory", (req, res) => {
+  const newItem = {
+    id: inventory.length + 1,
+    name: req.body.name,
+    category: req.body.category,
+    quantity: req.body.quantity,
+    status: req.body.status
+  };
+
+  inventory.push(newItem);
+  res.status(201).json(newItem);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
